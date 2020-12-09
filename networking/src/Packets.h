4a4bad6a435db81bb1e2ce112b271ba88129de1f
grillow <spmr757@gmail.com>
2020-12-09 13:48:46 +0300

    what is uint? (unsigned int)

--- networking/src/Packets.h
+++ networking/src/Packets.h
@@ -23,33 +23,33 @@ enum ObjectType : std::underlying_type_t<std::byte> {
 struct CreateObjectPacket : Packet {
     CreateObjectPacket& operator=(CreateObjectPacket&&);
     std::shared_ptr<Packet> ToPacket() const override;
-    CreateObjectPacket (const uint id, const ObjectType type, const std::vector<std::byte> binary);
-    static CreateObjectPacket Create(const uint id, const ObjectType type, const std::vector<std::byte> binary);
+    CreateObjectPacket (const unsigned int id, const ObjectType type, const std::vector<std::byte> binary);
+    static CreateObjectPacket Create(const unsigned int id, const ObjectType type, const std::vector<std::byte> binary);
     static CreateObjectPacket FromPacket(std::shared_ptr<Packet> Packet);
     
-    /*const*/ uint m_id;
+    /*const*/ unsigned int m_id;
     /*const*/ ObjectType m_object_type;
     /*const*/ std::vector<std::byte> m_object_binary;
 };
 struct ModifyObjectPacket : Packet {
     ModifyObjectPacket& operator=(ModifyObjectPacket&&);
     std::shared_ptr<Packet> ToPacket() const override;
-    ModifyObjectPacket (const uint id, const ObjectType type, const std::vector<std::byte> binary);
-    static ModifyObjectPacket Create(const uint id, const ObjectType type, const std::vector<std::byte> binary);
+    ModifyObjectPacket (const unsigned int id, const ObjectType type, const std::vector<std::byte> binary);
+    static ModifyObjectPacket Create(const unsigned int id, const ObjectType type, const std::vector<std::byte> binary);
     static ModifyObjectPacket FromPacket(std::shared_ptr<Packet> Packet);
     
-    /*const*/ uint m_id;
+    /*const*/ unsigned int m_id;
     /*const*/ ObjectType m_object_type;
     /*const*/ std::vector<std::byte> m_object_binary;
 };
 struct DeleteObjectPacket : Packet {
     DeleteObjectPacket& operator=(DeleteObjectPacket&&);
     std::shared_ptr<Packet> ToPacket() const override;
-    DeleteObjectPacket (const uint id);
-    static DeleteObjectPacket Create(const uint id);
+    DeleteObjectPacket (const unsigned int id);
+    static DeleteObjectPacket Create(const unsigned int id);
     static DeleteObjectPacket FromPacket(std::shared_ptr<Packet> Packet);
     
-    /*const*/ uint m_id;
+    /*const*/ unsigned int m_id;
 };
 
 enum EventType{}; //not my part
