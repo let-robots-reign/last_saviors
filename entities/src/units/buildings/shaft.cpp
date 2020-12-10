f60602eb3e63fbba82dadf5589e579711c817341
MKondakova <milanon@ya.ru>
2020-12-10 18:06:34 +0300

    delete comments

--- entities/src/units/buildings/shaft.cpp
+++ entities/src/units/buildings/shaft.cpp
@@ -1,24 +1,6 @@
 #include "shaft.h"
 
-#include <utility>
 
-// void Shaft::getCoins(Player *player, time_t current_time) {
-//    if (canGetCoins(current_time)) {
-//        player->increaseCoins(model_->amountOfCoinsPerLevel[level_]);
-//        time_of_last_usage_ = current_time;
-//    }
-//}
-// bool Shaft::canGetCoins(time_t current_time) {
-//    return current_time - time_of_last_usage_ >
-//           model_->cooldownPerLevel[level_];
-//}
-// Shaft::Shaft(const ShaftModel *model, time_t current_time, Coordinate
-// position,
-//             int level)
-//    : Unit(position),
-//      time_of_last_usage_(current_time),
-//      level_(level),
-//      model_(model) {}
 Shaft::Shaft(ShaftModel model, unsigned int current_time, Coordinate position,
              int level)
     : Unit(position),
