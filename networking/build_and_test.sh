7daf7ad27447481b9c8ff5d82eb18be2e3413b83
grillow <spmr757@gmail.com>
2020-12-10 17:02:02 +0300

    tests

--- networking/build_and_test.sh
+++ networking/build_and_test.sh
@@ -4,9 +4,9 @@ cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
 make || { echo 'MAKE failed' ; exit 1; }
 #../bin/networking || { echo 'PROGRAM failed' ; exit 1; }
 
-#cd ../test
-#cd build || mkdir build && cd build
-#cmake .. || { echo 'CMAKE failed' ; exit 1; }
-#make || { echo 'MAKE failed' ; exit 1; }
-#./networking_unit_tests || { echo 'TESTS failed' ; exit 1; }
+cd ../test
+cd build || mkdir build && cd build
+cmake .. || { echo 'CMAKE failed' ; exit 1; }
+make || { echo 'MAKE failed' ; exit 1; }
+./networking_unit_tests || { echo 'TESTS failed' ; exit 1; }
 
