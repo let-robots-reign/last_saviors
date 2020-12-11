22d1d68402fd4854b7f05d96a7292212deb11ed6
grillow <spmr757@gmail.com>
2020-12-12 02:16:02 +0300

    fixed cmake for tests

--- networking/build_and_test.sh
+++ networking/build_and_test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 cd ./networking_library
+#$(./build_and_test.sh) || { echo 'NETWORKING LIBRARY FAILED' ; exit 1; }
 ./build_and_test.sh
 cd ..
 
-echo ""
-echo "BUILDING GAME NETWORKING LIBRARY"
+echo ''
+echo 'BUILDING GAME NETWORKING LIBRARY'
 cd build || mkdir build && cd build
 
 cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
 make || { echo 'MAKE failed' ; exit 1; }
 
+cd ../test
+cd build || mkdir build && cd build
+cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
+make || { echo 'MAKE failed' ; exit 1; }
+./game_networking_tests || { echo 'TESTS failed' ; exit 1; }
 
--- networking/networking_library/build_and_test.sh
+++ networking/networking_library/build_and_test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo "BUILDING NETWORKING LIBRARY"
+echo 'BUILDING NETWORKING LIBRARY'
 cd build || mkdir build && cd build
 
 cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
 make || { echo 'MAKE failed' ; exit 1; }
+
+cd ../test
+cd build || mkdir build && cd build
+cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
+make || { echo 'MAKE failed' ; exit 1; }
+./networking_tests || { echo 'TESTS failed' ; exit 1; }
 
