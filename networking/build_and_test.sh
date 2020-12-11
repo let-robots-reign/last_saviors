4737a2139d86e753ecfe4fde48f807a5af483bbf
grillow <spmr757@gmail.com>
2020-12-12 02:35:27 +0300

    we need more bash scripts

--- networking/build_and_test.sh
+++ networking/build_and_test.sh
@@ -5,16 +5,5 @@ cd ./networking_library
 ./build_and_test.sh
 cd ..
 
-echo ''
-echo 'BUILDING GAME NETWORKING LIBRARY'
-cd build || mkdir build && cd build
-
-cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
-make || { echo 'MAKE failed' ; exit 1; }
-
-cd ../test
-cd build || mkdir build && cd build
-cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
-make || { echo 'MAKE failed' ; exit 1; }
-./game_networking_tests || { echo 'TESTS failed' ; exit 1; }
-
+./build.sh
+./test.sh
--- networking/networking_library/build_and_test.sh
+++ networking/networking_library/build_and_test.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-echo 'BUILDING NETWORKING LIBRARY'
-cd build || mkdir build && cd build
-
-cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
-make || { echo 'MAKE failed' ; exit 1; }
-
-cd ../test
-cd build || mkdir build && cd build
-cmake .. || { echo 'CMAKE FAILED' ; exit 1; }
-make || { echo 'MAKE failed' ; exit 1; }
-./networking_tests || { echo 'TESTS failed' ; exit 1; }
-
+./build.sh
+./test.sh
