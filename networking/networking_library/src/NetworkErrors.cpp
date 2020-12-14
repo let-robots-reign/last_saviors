9f259d6764c5afda5e75c4634632e5d715e66911
grillow <spmr757@gmail.com>
2020-12-14 20:43:30 +0300

    CMake CXX flags

--- networking/networking_library/src/NetworkErrors.cpp
+++ networking/networking_library/src/NetworkErrors.cpp
@@ -1,9 +1,9 @@
 #include "NetworkErrors.h"
 
 
-SocketError::SocketError(const int errno_, const char *what) : m_errno(errno_), std::runtime_error(what) {}
+SocketError::SocketError(const int errno_, const char *what) : std::runtime_error(what), m_errno(errno_) {}
 
 SocketDisconnect::SocketDisconnect(const int errno_) : SocketError(errno_, "Disconnected") {}
 
-SocketGracefulDisconnect::SocketGracefulDisconnect(const TCPSocketConnection &socket) : m_socket(socket), SocketDisconnect(0) {}
+SocketGracefulDisconnect::SocketGracefulDisconnect(const TCPSocketConnection &socket) : SocketDisconnect(0), m_socket(socket) {}
 
