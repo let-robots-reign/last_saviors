ec3c56246a282714b55fe4dd13b9c281edb9801f
Alexey Zotov <zotov.alexei.1@yandex.ru>
2020-12-27 18:09:42 +0400

    delete old networking

--- networking/build_and_test.sh
+++
@@ -1,8 +0,0 @@
-cmake . || { echo 'CMAKE FAILED' ; exit 1; }
-make || { echo 'MAKE failed' ; exit 1; }
-./networking || { echo 'PROGRAM failed' ; exit 1; }
-cd test
-cmake . || { echo 'CMAKE failed' ; exit 1; }
-make || { echo 'MAKE failed' ; exit 1; }
-./networking_unit_tests || { echo 'TESTS failed' ; exit 1; }
-
--- networking/src/Serializable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-
-class Serializable {
-public:
-    virtual std::vector<std::byte> ToBinary() const = 0;
-}
