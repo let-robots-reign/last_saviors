51b2131fcc732ecb7d3342ada16a3b3fd7875b9f
MKondakova <milanon@ya.ru>
2020-12-28 13:10:15 +0300

    add suppressions.txt

--- entities/test/test_unit.cpp
+++ entities/test/test_unit.cpp
@@ -6,3 +6,11 @@ TEST(TestUnit, distance) {
     Unit second(Coordinate(1, 1));
     ASSERT_DOUBLE_EQ(first.distance(second), std::sqrt(10));
 }
+
+TEST(TestUnit, setCoordinate) {
+    Unit unit((Coordinate()));
+    Coordinate new_cords = Coordinate(1, 1);
+    unit.setCoordinate(new_cords);
+    EXPECT_EQ(unit.getCoordinate().getX(), new_cords.getX());
+    EXPECT_EQ(unit.getCoordinate().getY(), new_cords.getY());
+}
